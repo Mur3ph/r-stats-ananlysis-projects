65a6504bbc77ea50c582556a4aa80368b405242d
Mur3ph <paulmurphymainstreet@gmail.com>
2016-08-30 15:54:31 +0100

    Getting the statistical averages from the data

--- R-Tutorial-Canada-Crime/canada-crime.r
+++ R-Tutorial-Canada-Crime/canada-crime.r
@@ -5,23 +5,37 @@
 setwd("C:/dev/R/workspace/R-Tutorial-Canada-Crime/data")
 
 # Read the data
-mydata<- read.csv("C:/dev/R/workspace/R-Tutorial-Canada-Crime/data/Hartnagel.csv")
+crime_data <- read.csv("C:/dev/R/workspace/R-Tutorial-Canada-Crime/data/Hartnagel.csv")
 
 # View data in seperate window
-#View(mydata)
+# View(crime_data)
 
 # Show first lines of data
-head(mydata)
-mydata[1:10,]
+head(crime_data )
+crime_data[1:10,]
 
 #Position of data as a vector row 1 column 4
-mydata[1,4]
+crime_data[1,4]
 
-
-x <- subset(year, ftheft < mtheft)
+# Print out year according to conditional statement
+x <- subset(crime_data, ftheft < mtheft)
 # print(paste0("Year female theft was greater than male theft ", x))
-sprintf("Year female theft was greater than male theft ", x)
-# cat("Year female theft was greater than male theft", x)
+# sprintf("Year female theft was greater than male theft ", x)
+cat("Year female theft was greater than male theft", x)
 
+# Getting statistical averages from data
+mean(crime_data$fconvict)
+mean(crime_data$mtheft, na.rm = TRUE) # Removes the NA from the column and then calculates the mean
 
+median(crime_data$fconvict)
+median(crime_data$mtheft, na.rm = TRUE) # Removes the NA from the column and then calculates the median
+
+quantile(crime_data$fconvict)
+quantile(crime_data$mtheft, na.rm = TRUE) # Removes the NA from the column and then calculates the quantile
+
+var(crime_data$fconvict)
+var(crime_data$mtheft, na.rm = TRUE) # Removes the NA from the column and then calculates the variance
+
+sd(crime_data$fconvict)
+sd(crime_data$mtheft, na.rm = TRUE) # Removes the NA from the column and then calculates the standard deviation
 
